3d23155614f775f4c27f3e1edd2ba38b2388e570
Michal Buszkiewicz <embed1@gmail.com>
2018-07-28 17:01:36 +0200

    Add comment functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'comments/create'
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
 
-  resources :articles
+  resources :articles do
+    resources :comments, only: [:create]
+  end
 
 end
