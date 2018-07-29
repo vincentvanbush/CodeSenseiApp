451fe7cc315e72f02892c90bda48fd3bd97758ac
Marek Selzer <mselzer@o2.pl>
2018-07-29 12:16:06 +0200

    Add comment deletion feature

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :articles do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
+
   end
 
 end
