87254251d0b7cb93bf131f4869af644e4201fb09
Michal Buszkiewicz <embed1@gmail.com>
2018-07-29 12:16:04 +0200

    Add comment deletion feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,18 @@
 class CommentsController < ApplicationController
   def create
     article = Article.find(params[:article_id])
-    comment = article.comments.new(comment_params)
+    comment = article.comments.build(comment_params)
     comment.save
     redirect_to article_path(article)
   end
+
+  def destroy
+    article = Article.find(params[:article_id])
+    comment = article.comments.find(params[:id])
+    comment.destroy
+    redirect_to article_path(article)
+  end
+
 
   private
 
