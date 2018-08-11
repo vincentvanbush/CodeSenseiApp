4b92ce9ad624aa3594ef1c56ae1a86767b76d45a
Czerwonka <alicja.czerwonka@gmail.com>
2018-08-11 11:00:40 +0200

    Add pry gems

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  
+  http_basic_authenticate_with name:"admin",password:"secret",except:[:index,:show]
+
+
   def new
     @article = Article.new
   end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,4 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 5, maximum: 20 }
-end
+end		
