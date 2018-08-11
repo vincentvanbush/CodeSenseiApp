019127a506de2d5ce49eb40378ae31c1e8e6348b
Dzemila Murzicz <dz.murzicz@gmail.com>
2018-08-11 15:43:12 +0200

    Tweak forms

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -57,6 +57,6 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :text, :article_type)
   end
 end
