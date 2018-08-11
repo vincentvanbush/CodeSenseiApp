87eed8b0b337279756ff6d3257c9751f95158f1f
Czerwonka <alicja.czerwonka@gmail.com>
2018-08-11 13:57:35 +0200

    Add simple_form and article form

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 5, maximum: 20 }
+  validates :text, presence: true
+
 end		
