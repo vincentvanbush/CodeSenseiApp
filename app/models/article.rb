b18353b509142a69ff9f1416b4878cbae03ad02d
Marek Selzer <mselzer@o2.pl>
2018-08-12 13:53:54 +0200

    ee

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ApplicationRecord
   enum article_type: [:normal, :sponsored]
-  
+
   has_many :comments, dependent: :destroy
+  belongs_to :user
+
 
   validates :title, presence: true, length: { minimum: 5, maximum: 20 }
   validates :text, presence: true
