fd972200c1fb29423d7d726c53a8e46d6aab3a02
dendtis <dendtis@aol.com>
2018-08-12 11:36:29 +0200

    Add login and flash info to main layout

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,16 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with name: "admin", password: "secret", only: :destroy
-
   def create
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.build(comment_params)
-    if @comment.save
-      redirect_to article_path(@article)
-    else
-      render 'articles/show'
+  	@article = Article.find(params[article_id])
+  	@comment = article.comments.new(comment_params)
+  	if @comment.save
+      flash[:notice] = 'Your comment was saved'
+  	  redirect_to article_path(@article) 
     end
-  end
-
-  def destroy
-    article = Article.find(params[:article_id])
-    comment = article.comments.find(params[:id])
-    comment.destroy
-    redirect_to article_path(article)
-  end
-
 
   private
 
   def comment_params
-    params.require(:comment).permit(:commenter, :body)
+  	params.require(:comment).permit(:commenter, :body)
   end
+
 end
