31f6745bc970d135ca6e3689b7ca18611f542fd6
Czerwonka <alicja.czerwonka@gmail.com>
2018-08-12 13:53:32 +0200

    Add relationship between user and article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_11_142107) do
+ActiveRecord::Schema.define(version: 2018_08_12_101525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2018_08_11_142107) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "article_type", default: 0
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -49,5 +51,6 @@ ActiveRecord::Schema.define(version: 2018_08_11_142107) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
 end
