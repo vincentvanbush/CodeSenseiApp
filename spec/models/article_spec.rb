5ace1b586c6cff2e0e6ac58eaf5602d7898204a0
Michał Buszkiewicz <embed1@gmail.com>
2018-09-09 15:24:39 +0200

    Add RSpec and tests for Article

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Article do
+  subject(:article) do
+    Article.new(title: title, text: text, user: user)
+  end
+
+  let(:text) { 'lorem ipsum' }
+
+  let(:user) { User.create(email: 'qwer@asdf.pl', password: 'qwer1234') }
+
+  context 'when title does not exist' do
+    let(:title) { nil }
+
+    it { is_expected.not_to be_valid }
+  end
+
+  context 'when title has fewer than 5 characters' do
+    let(:title) { 'asdf' }
+
+    it { is_expected.not_to be_valid }
+  end
+
+  context 'when title has 5 characters' do
+    let(:title) { 'asdfg' }
+
+    it { is_expected.to be_valid }
+  end
+
+  context 'when title has 20 characters' do
+    let(:title) { 'asdfgasdfgasdfgasdfg' }
+
+    it { is_expected.to be_valid }
+  end
+
+  context 'when title has 21 characters' do
+    let(:title) { 'asdfgasdfgasdfgasdfgw' }
+
+    it { is_expected.not_to be_valid }
+  end
+end
