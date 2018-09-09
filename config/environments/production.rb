183c6711a647596b69c55857fccb59ae24ecffad
Marek Selzer <mselzer@o2.pl>
2018-09-09 11:30:16 +0200

    mailer poprawiony znowu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :sendgrid_actionmailer
 
   config.action_mailer.sendgrid_actionmailer_settings = {
-    api_key: ENV['Sendgrid_API_Key'],
+    api_key: ENV['SENDGRID_API_KEY'],
     raise_delivery_errors: true
   }
 
